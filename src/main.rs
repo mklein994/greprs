191febc3593eab24c96f3609a45602969da97dcd
Matthew Klein <mklein994@gmail.com>
2017-07-16 17:32:47 -0500

    Print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 extern crate greprs;
 
-use std::process;
 use std::env;
+use std::process;
+use std::io::prelude::*;
 
 use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        writeln!(
+            &mut stderr,
+            "Problem parsing arguments: {}",
+            err
+        ).expect("Could not write to stderr");
         process::exit(1);
     });
 
